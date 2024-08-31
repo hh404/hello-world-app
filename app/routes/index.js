afbc280707195708fca7106bfc89f0505c9256fd
hh404 <hjw231_2003@sina.com>
2024-08-31 22:45:59 +0800

    [emberjs]: uer and params

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,16 @@
+
+
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-  queryParams = {
-    country: { refreshModel: true },
-    language: { refreshModel: true },
-    username: { refreshModel: true }
-  };
+  @service store;
 
   model(params) {
-    return {
+    return this.store.createRecord('user', {
       country: params.country,
       language: params.language,
       username: params.username
-    };
+    });
   }
 }
